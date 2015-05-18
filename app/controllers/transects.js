76b1a97ea7f71e159303113ce04eaac99265614a
stapler343 <stapler343@hotmail.com>
2015-05-18 16:38:44 -0700

    try using add button in transects to directly open quadrats

--- app/controllers/transects.js
+++ app/controllers/transects.js
@@ -326,11 +326,13 @@ function showTotalRowNumber(){
 function addBtn(){
 	//disable add button until screen is returned to focus.  Issue #28
 	$.addTransect.enabled = false;
-	
-	
-	var addTransect = Alloy.createController("addTransect", {siteID: $.tbl.siteID, protocolName: $.tbl.protocolName}).getView();
+
+	var quadrats = Alloy.createController("quadrats", {transectID:e.rowData.transectID, siteID:$.tbl.siteID}).getView();
 	var nav = Alloy.Globals.navMenu;
-	nav.openWindow(addTransect);
+	nav.openWindow(quadrats);
+	//var addTransect = Alloy.createController("addTransect", {siteID: $.tbl.siteID, protocolName: $.tbl.protocolName}).getView();
+	//var nav = Alloy.Globals.navMenu;
+	//nav.openWindow(addTransect);
 }
 
 // Delete a file from the application data directory
