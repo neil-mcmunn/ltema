b4d4d9749f2a700fba41b9919d901bdc61b00d3b
Neil McMunn <neil.mcmunn@gmail.com>
2015-06-05 13:56:51 -0700

    Query stuff

--- app/lib/upload.js
+++ app/lib/upload.js
@@ -3,6 +3,11 @@
 var uuid = require('uuid.js');
 var OAuthSig = require('oauth-signature');
 
+Ti.App.addEventListener("app:dataBaseError", function(e) {
+	//TODO: handle a database error for the app
+	Titanium.API.error("Database error: " + e.error);
+});
+
 function preparePhotos(guid) {
 	try{
 		var db = Ti.Database.open('ltemaDB');
@@ -129,7 +134,7 @@ function uploadPhotos (media, guid, callback){
 
 		//DO: Grab thesse from the cloud instead of hardcoding
 		//access token
-		var access_token = Ti.App.Properties.getString('access_token');;
+		var access_token = Ti.App.Properties.getString('access_token');
 		var access_secret = Ti.App.Properties.getString('access_secret');
 		
 		var consumer_key = Ti.App.Properties.getString('consumer_key');
@@ -245,7 +250,7 @@ function formAlpineGrasslandJSON(guid){
 		var db = Ti.Database.open('ltemaDB');
 	
 		//Query - Select metadata for the survey being uploaded
-		var rows = db.execute( 'SELECT site_survey_guid, year, protocol_id, park_id \
+		var metaRows = db.execute( 'SELECT site_survey_guid, year, protocol_id, park_id \
 								FROM site_survey \
 								WHERE site_id = ? AND protocol_id = ?', siteName, protocolName); //TODO: MAKE SURE THESE NUMBERS ARE WHAT WE WANT
 
@@ -255,12 +260,96 @@ function formAlpineGrasslandJSON(guid){
 			plot_observations : [],
 			media : [],
 			survey_meta : {
-				site_survey_guid : rows.fieldByName('site_survey_guid'),
-				year : rows.fieldByName('year'),
-				protocol_id : rows.fieldByName('protocol_id'),
-				park_id : rows.fieldByName('park_id')
+				site_survey_guid : metaRows.fieldByName('site_survey_guid'),
+				year : metaRows.fieldByName('year'),
+				protocol_id : metaRows.fieldByName('protocol_id'),
+				park_id : metaRows.fieldByName('park_id')
 			}
 		}
+
+		var transectRows = db.execute( 'SELECT site_survey_guid, year, protocol_id, park_id \
+										FROM site_survey \
+										WHERE site_id = ? AND protocol_id = ?', siteName, protocolName); //TODO: MAKE SURE THESE NUMBERS ARE WHAT WE WANT
+		while(transectRows.isValidRow()){
+			var transect = {
+				transect_id: transectRows.fieldByName('transect_id'),
+				transect_guid: transectRows.fieldByName('transect_guid'),
+				transect_name: transectRows.fieldByName('transect_name'),
+				surveyor: transectRows.fieldByName('surveyor'),
+				other_surveyors: transectRows.fieldByName('other_surveyors'),
+				plot_distance: transectRows.fieldByName('plot_distance'),
+				stake_orientation: transectRows.fieldByName('stake_orientation'),
+				utm_zone: transectRows.fieldByName('utm_zone'),
+				utm_easting: transectRows.fieldByName('utm_easting'),
+				utm_northing: transectRows.fieldByName('utm_northing'),
+				comments: transectRows.fieldByName('comments'),
+				site_ids: transectRows.fieldByName('site_ids'),
+				site_survey_guid: transectRows.fieldByName('site_survey_guid'),
+				media_id: transectRows.fieldByName('media_id')
+			};
+			survey.transects.push(transect);
+			transectRows.next();
+		}
+
+		var plotRows = db.execute( 'SELECT site_survey_guid, year, protocol_id, park_id
+									FROM site_survey
+									WHERE site_id = ? AND protocol_id = ?', siteName, protocolName); //TODO: MAKE SURE THESE NUMBERS ARE WHAT WE WANT
+		while(plotRows.isValidRow()){
+			var plot = {
+				plot_id: plotRows.fieldByName('plot_id'),
+				plot_guid: plotRows.fieldByName('plot_guid'),
+				plot_name: plotRows.fieldByName('plot_name'),
+				utm_zone: plotRows.fieldByName('utm_zone'),
+				utm_northing: plotRows.fieldByName('utm_northing'),
+				utm_easting: plotRows.fieldByName('utm_easting'),
+				utc: plotRows.fieldByName('utc'),
+				stake_deviation: plotRows.fieldByName('stake_deviation'),
+				distance_deviation: plotRows.fieldByName('distance_deviation'),
+				transect_id: plotRows.fieldByName('transect_id'),
+				transect_guid: plotRows.fieldByName('transect_guid'),
+				media_id: plotRows.fieldByName('media_id'),
+				comment: plotRows.fieldByName('comment')
+			};
+			survey.plots.push(plot);
+			plotRows.next();
+		}
+
+		var observationRows = db.execute(	'SELECT ob.observation_id, ob.plot_observation_guid, ob.observation,
+											 ob.ground_cover, ob.count, ob.comments, ob.plot_id, ob.plot_guid, ob.media_id, ob.species_code
+											 FROM plot_observation ob, plot pl, transect t, survey s
+											 WHERE ob.plot_id = pl.plot_id
+											 AND pl.transect_id = t.transect_id
+											 AND ', guid); //TODO: MAKE SURE THESE NUMBERS ARE WHAT WE WANT
+		while(observationRows.isValidRow()){
+			var observation = {
+				observation_id: observationRows.fieldByName('observation_id'),
+				plot_observation_guid: observationRows.fieldByName('plot_observation_guid'),
+				observation: observationRows.fieldByName('observation'),
+				ground_cover: observationRows.fieldByName('ground_cover'),
+				count: observationRows.fieldByName('count'),
+				comments: observationRows.fieldByName('comments'),
+				plot_id: observationRows.fieldByName('plot_id'),
+				plot_guid: observationRows.fieldByName('plot_guid'),
+				media_id: observationRows.fieldByName('media_id'),
+				species_code: observationRows.fieldByName('species_code')
+			};
+			survey.plot_observations.push(observation);
+			observationRows.next();
+		}
+
+		var mediaRows = db.execute(	'SELECT site_survey_guid, year, protocol_id, park_id \
+									 FROM site_survey \
+									 WHERE site_id = ? AND protocol_id = ?', siteName, protocolName); //TODO: MAKE SURE THESE NUMBERS ARE WHAT WE WANT
+		while(mediaRows.isValidRow()){
+			var media = {
+				media_id: mediaRows.fieldByName('media_id'),
+				media_name: mediaRows.fieldByName('media_name'),
+				flickr_id: mediaRows.fieldByName('flickr_id')
+			};
+			survey.media.push(media);
+			mediaRows.next();
+		}
+
 	}
 	catch(e) {
 		var errorMessage = e.message;
@@ -272,7 +361,7 @@ function formAlpineGrasslandJSON(guid){
 	}
 }
 
-function uploadJSON() {
+function uploadJSON(survey, guid) {
 	
 }
 
@@ -283,3 +372,15 @@ function formIntertidalJSON(){
 	
 }
 */
+
+//Assess if all the propertiees of an object have been assigned
+function varsAssigned(object){
+	for (var prop in object){
+		if(prop === undefined){
+			return false;
+		}
+	}
+
+	return true;
+}
+
